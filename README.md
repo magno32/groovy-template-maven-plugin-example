adb150e055fac9f08eb20e375f243111ff77155b
Justin Smith <justin.smith@summitsystemsinc.com>
2016-02-10 03:09:29 -0600

    Add example project.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# groovy-template-maven-plugin-example
-Example usage for the groovy-template Maven Plugin
+# Example for Groovy-Template Maven Plugin
+
+This is an example project that uses the [Groovy Template Maven Plugin](https://github.com/magno32/groovy-template-maven-plugin).
+
+For full project documentation please see the [project docs](http://magno32.github.io/groovy-template-maven-plugin).
+
+## Usage
+To test this example: 
+
+1. Clone this repository
+2. Execute `mvn package`
+3. View generated sources in `target/generated-sources`
---
+++ src/main/gtemplate/com/summitsystemsinc/maven/ExampleMain.java
@@ -0,0 +1,33 @@
+/*
+ * #%L
+ * Groovy Template :: Maven Plugin :: Example
+ * %%
+ * Copyright (C) 2016 Summit Management Systems, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ *
+ * @author Justin Smith
+ */
+public class ExampleMain {
+	public static void main(String[] args){
+		<% for(def m : messages) { %>
+			//Printing "$m" to console.
+			System.out.println("$m");
+		<% } %>
+		
+		System.out.println("Property from pom: foo=$foo");
+	}
+}
